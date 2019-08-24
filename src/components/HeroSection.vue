2865a3a9dd419b64d8e9f1ddfd12089985823071
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-08-24 14:48:03 +0530

    Favicon

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="HeroSection"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HeroSection",
+  components: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeroSection />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeroSection from "@/components/HeroSection.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    Home
   }
-}
+};
 </script>
