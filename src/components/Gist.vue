cd40eb048d24263385274ad4991e04ac88f3e22b
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-08-28 22:34:41 +0530

    Meta images added

---
+++ src/components/Gist.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="gist">
+    <div class="gist-card-red"></div>
+    <!-- <div class="gist-card-blue"></div> -->
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.gist {
+  padding: 3%;
+
+  .gist-card-red {
+    height: 300px;
+    border-radius: 10px;
+    width: 400px;
+    box-shadow: 5px 5px 30px -10px #bd180678;
+    background: linear-gradient(#ed213a, #93291e);
+    &:hover {
+      box-shadow: 15px 15px 30px -10px #bd1806ab;
+    }
+    transition: all 0.3s linear;
+  }
+  .gist-card-blue {
+    height: 300px;
+    border-radius: 10px;
+    width: 400px;
+    box-shadow: 10px 10px 50px #021b79;
+    background: linear-gradient(#0575e6, #0228af);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
     <HeroSection />
+    <Gist />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeroSection from "@/components/HeroSection.vue";
-
+import Gist from "@/components/Gist.vue";
 export default {
   name: "home",
   components: {
-    HeroSection
+    HeroSection,
+    Gist
   }
 };
 </script>
+<style lang="scss">
+.home {
+  background: #232323;
+}
+</style>
